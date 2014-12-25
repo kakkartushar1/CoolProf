3406b03407154619c5d1a3caf1b76da2c6c17ef5
rubcuevas <rubencuevas93@gmail.com>
2014-12-25 21:14:36 +0100

    Implemented top bio terms

--- data_extraction/coolfunctions.py
+++ data_extraction/coolfunctions.py
@@ -4,34 +4,296 @@ import pandas as pd
 from collections import defaultdict
 from pymongo import MongoClient
 
+STOP_WORDS = """de
+la
+que
+el
+en
+y
+a
+los
+del
+se
+las
+por
+un
+para
+con
+no
+una
+su
+al
+es
+lo
+como
+más
+pero
+sus
+le
+ya
+o
+fue
+este
+ha
+sí
+porque
+esta
+son
+entre
+está
+cuando
+muy
+sin
+sobre
+ser
+tiene
+también
+me
+hasta
+hay
+donde
+han
+quien
+están
+estado
+desde
+todo
+nos
+durante
+estados
+todos
+uno
+les
+ni
+contra
+otros
+fueron
+ese
+eso
+había
+ante
+ellos
+e
+esto
+mí
+antes
+algunos
+qué
+unos
+yo
+otro
+otras
+otra
+él
+tanto
+esa
+estos
+mucho
+quienes
+nada
+muchos
+cual
+sea
+poco
+ella
+estar
+haber
+estas
+estaba
+estamos
+algunas
+algo
+nosotros
+mi
+mis
+tú
+te
+ti
+tu
+tus
+ellas
+nosotras
+vosotros
+os
+mío
+mía
+míos
+mías
+tuyo
+tuya
+tuyos
+suyo
+suya
+suyos
+nuestro
+nuestra
+nuestros
+nuestras
+vuestro
+vuestra
+vuestros
+esos
+esas
+estoy
+estás
+está
+estamos
+estáis
+están
+esté
+estés
+estemos
+estén
+estaré
+estarás
+estará
+estaremos
+estarán
+estaría
+estarías
+estaríamos
+estarían
+estaba
+estabas
+estábamos
+estaban
+estuve
+estuviste
+estuvo
+estuvimos
+estuvieron
+estuviera
+estuvieras
+estando
+estado
+estados
+he
+has
+ha
+hemos
+habéis
+han
+haya
+hayas
+hayamos
+hayan
+habrá
+habrán
+habría
+habrías
+habrían
+había
+habías
+habíamos
+habían
+hubo
+hubiera
+hubieras
+hubieran
+hubiese
+habido
+soy
+eres
+es
+somos
+sois
+son
+sea
+seas
+seamos
+sean
+seré
+serás
+será
+seremos
+serán
+sería
+serías
+serían
+era
+eras
+éramos
+eran
+fui
+fuiste
+fue
+fuimos
+fueron
+fuera
+fueras
+fueran
+fuese
+siendo
+sido
+tengo
+tienes
+tiene
+tenemos
+tenéis
+tienen
+tenga
+tengas
+tengamos
+tengan
+tendré
+tendrás
+tendrá
+tendremos
+tendrán
+tendría
+tendrías
+tendríamos
+tenía
+tenías
+teníamos
+tenían
+tuve
+tuviste
+tuvo
+tuvimos
+tuvieron
+tuviera
+tuvieras
+teniendo
+tenido
+""".split("\n")
 
-def full_name_terms_to_csv(freqs, file_path):
+def top_terms_to_csv(freqs, file_path):
     with open(file_path, 'w') as f:
         f.write("token;frequency\n")
         for token, freq in freqs.iteritems():
             f.write(token + ";" + str(freq) + "\n")
 
-
-def top_full_name_terms(server, port, dbname, dbcol, num_min_users):
+def top_terms_user_data(field, server, port, dbname, dbcol, num_min_users, remove_stop_words):
     cooltweets = MongoClient(server, port)[dbname][dbcol]
     tokfreqs = pd.Series()
     tokusers = defaultdict(set)
-    names = cooltweets.find({}, {"_id": 0, "name": 1, "user.name": 1, "user.screen_name": 1})
-    for entry in names:
-        name = entry["user"]["name"]
+    rows = cooltweets.find({}, {"_id": 0, "name": 1, "user.name": 1, "user.screen_name": 1, "user.description": 1})
+    for entry in rows:
+        data = entry["user"][field]
         user = entry["user"]["screen_name"]
         # Get tokens by replacing non-alphanumeric characters by spaces and splitting
-        for token in "".join([c if c.isalnum() else " " for c in name.lower()]).split():
-            # Discard tokens with numbers in them
-            if all([c.isalpha() for c in token]):
-                tokusers[token].add(user)
-                if token in tokfreqs:
-                    tokfreqs[token] += 1
-                else:
-                    tokfreqs[token] = 1
+        for token in "".join([c if c.isalnum() else " " for c in data.lower()]).split():
+
+            if not remove_stop_words or token not in STOP_WORDS:
+                # Discard tokens with numbers in them
+                if all([c.isalpha() for c in token]):
+                    tokusers[token].add(user)
+                    if token in tokfreqs:
+                        tokfreqs[token] += 1
+                    else:
+                        tokfreqs[token] = 1
 
     return pd.Series({
         token: freq for token, freq in tokfreqs.iteritems() if len(tokusers[token]) >= num_min_users})
+
+
+def top_full_name_terms(server, port, dbname, dbcol, num_min_users):
+    return top_terms_user_data("name", server, port, dbname, dbcol, num_min_users, False)
+
+
+def top_bio_terms(server, port, dbname, dbcol, num_min_users):
+    return top_terms_user_data("description", server, port, dbname, dbcol, num_min_users, True)
 
 
 
--- data_extraction/test/__init__.py
+++ data_extraction/test/__init__.py
@@ -10,22 +10,38 @@ DBNAME = "test_cooldb"
 COLNAME = "test_cooltweets"
 TOKENS_FREQ_FILE = "/Users/rubcuevas/coolprof/tokens_freq.csv"
 NUM_MIN_USERS = 2
-tweets = [{"user": {"name": "Rubén-Cuevas Ruben9", "screen_name": "rubcuevas"}},
-                    {"user": {"name": "Rubén      Cuevas", "screen_name": "rubcuevas"}},
-                    {"user": {"name": "Rubén", "screen_name": "rubcuevas"}},
-                    {"user": {"name": " Rubén\u2318", "screen_name": "rcuevas33"}},
-                    {"user": {"name": "Pepe ", "screen_name": "kiki"}},
-                    {"user": {"name": " Pepe ", "screen_name": "lulu"}},
-                    {"user": {"name": "Ruben", "screen_name": "rubcuevas"}},
-                    {"user": {"name": "Ruben", "screen_name": "rubengarcia"}},
-                    {"user": {"name": "¿¿¿Ruben???...Ruben..", "screen_name": "garciaruben"}},
-                    {"user": {"name": "Ruben", "screen_name": "tututu"}},
-                    {"user": {"name": "\u2318 ¿!//Ruben??!", "screen_name": "messi"}},
-                    {"user": {"name": "Mario \u2318", "screen_name": "mmario"}},
-                    {"user": {"name": "Jeje \u2318", "screen_name": "00mario00"}},
-                    {"user": {"name": "Mario", "screen_name": "mmario"}},
-                    {"user": {"name": "D4niel", "screen_name": "dani1"}},
-                    {"user": {"name": "D4niel", "screen_name": "dani2"}},
+tweets = [{"user": {"name": "Rubén-Cuevas Ruben9",
+                    "screen_name": "rubcuevas", "description": "Soy español y me gusta el deporte"}},
+                    {"user": {"name": "Rubén      Cuevas",
+                              "screen_name": "rubcuevas", "description": ""}},
+                    {"user": {"name": "Rubén",
+                              "screen_name": "rubcuevas", "description": ""}},
+                    {"user": {"name": " Rubén\u2318", "screen_name": "rcuevas33",
+                              "description": "Español, I \u2665 fiesta y deporte"}},
+                    {"user": {"name": "Pepe ",
+                              "screen_name": "kiki", "description": "Soy adicto al deporte"}},
+                    {"user": {"name": " Pepe ",
+                              "screen_name": "lulu", "description": "Padre orgulloso"}},
+                    {"user": {"name": "Ruben", "screen_name": "rubcuevas",
+                              "description": ""}},
+                    {"user": {"name": "Ruben",
+                              "screen_name": "rubengarcia", "description": "Esposo y padre"}},
+                    {"user": {"name": "¿¿¿Ruben???...Ruben..",
+                              "screen_name": "garciaruben", "description": "Soy informático y tengo un hijo. Puxa Asturies!!!."}},
+                    {"user": {"name": "Ruben",
+                              "screen_name": "tututu", "description": ""}},
+                    {"user": {"name": "\u2318 ¿!//Ruben??!", "screen_name": "messi",
+                              "description": ""}},
+                    {"user": {"name": "Mario \u2318", "screen_name": "mmario",
+                              "description": ""}},
+                    {"user": {"name": "Jeje \u2318", "screen_name": "00mario00",
+                              "description": ""}},
+                    {"user": {"name": "Mario", "screen_name": "mmario",
+                              "description": ""}},
+                    {"user": {"name": "D4niel", "screen_name": "dani1",
+                              "description": ""}},
+                    {"user": {"name": "D4niel", "screen_name": "dani2",
+                              "description": ""}}
           ]
 
 class TestCoolfunctions(unittest.TestCase):
@@ -41,7 +57,7 @@ class TestCoolfunctions(unittest.TestCase):
         db.drop_collection(COLNAME)
 
 
-    def test_tokens_freq(self):
+    def test_top_full_name_terms(self):
         tokens_freq = cf.top_full_name_terms(HOST, PORT, DBNAME, COLNAME, NUM_MIN_USERS)
         solution = {
             "rubén": 4,
@@ -51,8 +67,19 @@ class TestCoolfunctions(unittest.TestCase):
         }
         self.assertEquals(solution, tokens_freq.to_dict())
 
+    def test_top_bio_terms(self):
+        tokens_freq = cf.top_bio_terms(HOST, PORT, DBNAME, COLNAME, NUM_MIN_USERS)
+        solution = {
+            "deporte": 3,
+            "padre": 2,
+            "español": 2
+
+        }
+        self.assertEquals(solution, tokens_freq.to_dict())
+
+
     def test_write_tokens_freq(self):
-        cf.full_name_terms_to_csv(cf.top_full_name_terms(
+        cf.top_terms_to_csv(cf.top_full_name_terms(
            HOST, PORT, DBNAME, COLNAME, NUM_MIN_USERS
         ), TOKENS_FREQ_FILE)
         freqs = pd.read_csv(TOKENS_FREQ_FILE, sep=";")
@@ -60,6 +87,8 @@ class TestCoolfunctions(unittest.TestCase):
         self.assertEquals(freqs[freqs["token"] == "rubén"].iloc[0]["frequency"], 4)
         self.assertEquals(freqs[freqs["token"] == "pepe"].iloc[0]["frequency"], 2)
         self.assertEquals(len(freqs.index), 3)
+
+
 
 
 
